da02fbc1eea7e0d18aa87c3b0dac1295f3b58f80
Nick Balestra <nbalestra@opentable.com>
2017-07-30 16:01:18 +0100

    minimal registry setup

--- registry/index.js
+++ registry/index.js
@@ -1,7 +1,28 @@
-const http = require('http')
-const bunny = require('sign-bunny')
-http.createServer((req, res) => {
-  console.log(process.env)
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-  res.end(bunny('Foo'))
-}).listen(process.env.PORT || 3000)
+const oc = require('oc');
+
+const configuration = {
+  verbosity: 0,
+  baseUrl: 'https://opencomponents-site-registry.herokuapp.com/',
+  port: process.env.PORT || 3000,
+  tempDir: './temp/',
+  refreshInterval: 600,
+  pollingInterval: 5,
+  s3: {
+    key: process.env.S3KEY,
+    secret: process.env.S3SECRET,
+    bucket: process.env.S3BUCKET,
+    region: process.env.S3REGION,
+    path: `//s3.amazonaws.com/${process.env.S3BUCKET}/`,
+    componentsDir: 'components'
+  },
+  env: { name: 'production' }
+};
+
+const registry = new oc.Registry(configuration);
+
+registry.start(function(err, app){
+  if(err){
+    console.log('Registry not started: ', err);
+    process.exit(1);
+  }
+});

ef74c04a35a4055ad66df12f002890149a942224
Bruna Lima <missbruni@users.noreply.github.com>
2022-03-11 09:45:48 +0000

    add to theme

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
       }),
     ],
   ],
-
+  themes: ['@docusaurus/theme-search-algolia'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({

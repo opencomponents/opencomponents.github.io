395c1fef29921410301ed32b1fdd89b5186f6232
Ricardo Devis Agullo <mkredz@gmail.com>
2024-05-04 21:18:13 +0200

    use browseronly

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,19 +7,23 @@ import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 import LogoUrl from "@site/static/img/logo.png";
 import UwuLogoUrl from "@site/static/img/uwulogo.png";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  const uwu = window.location.search.includes("uwu");
 
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        {uwu ? (
-          <img alt="OC uwu logo" src={UwuLogoUrl} />
-        ) : (
-          <h1 className="hero__title">{siteConfig.title}</h1>
-        )}
+        <BrowserOnly>
+          {() =>
+            window.location.search.includes("uwu") ? (
+              <img alt="OC uwu logo" src={UwuLogoUrl} />
+            ) : (
+              <h1 className="hero__title">{siteConfig.title}</h1>
+            )
+          }
+        </BrowserOnly>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link

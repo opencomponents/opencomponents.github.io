fcdaffed560203e42470b6c8ed73d479116b4625
Ricardo Devis Agullo <mkredz@gmail.com>
2024-05-04 17:46:41 +0200

    add uwu

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,13 +6,20 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 import LogoUrl from "@site/static/img/logo.png";
+import UwuLogoUrl from "@site/static/img/uwulogo.png";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+  const uwu = new URLSearchParams(window.location.search).get("uwu") !== null;
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        {uwu ? (
+          <img alt="OC uwu logo" src={UwuLogoUrl} />
+        ) : (
+          <h1 className="hero__title">{siteConfig.title}</h1>
+        )}
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
